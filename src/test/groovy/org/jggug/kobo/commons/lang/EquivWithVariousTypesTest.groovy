57d2cdd1018d4d3ef3e4344cda8037117fa37427
Y.Nakano <ynak@jggug.org>
2009-10-26 16:23:42 +0900

    added test cases of using various types: EquivWithVariousTypesTest

---
+++ src/test/groovy/org/jggug/kobo/commons/lang/EquivWithVariousTypesTest.groovy
@@ -0,0 +1,88 @@
+package org.jggug.kobo.commons.lang
+
+import org.jggug.kobo.commons.lang.Equiv
+
+class EquivWithVariousTypesTest extends GroovyTestCase {
+
+    static class Sample {
+        static Object OBJECT = new Object()
+        @Equiv String string = "DEFAULT"
+        @Equiv Integer integer = 1234
+        @Equiv Map map = [default:'DEFAULT', abc:'‚ ‚¢‚¤‚¦‚¨']
+        @Equiv List list = ['default', "‚ ‚¢‚¤‚¦‚¨"]
+        @Equiv Set set = ['default', "‚ ‚¢‚¤‚¦‚¨"] as Set
+        @Equiv Object object = OBJECT
+    }
+
+    def s0, s1, s2, s3, s4, s5, s6, s7
+
+    void setUp() {
+        s0 = new Sample()
+        s1 = new Sample()
+        s2 = new Sample(string:'MODIFIED')
+        s3 = new Sample(integer:777)
+        s4 = new Sample(map:[mod:'MODIFIED'])
+        s5 = new Sample(list:['MODIFIED'])
+        s6 = new Sample(set:['MODIFIED'] as Set)
+        s7 = new Sample(object:new Object())
+    }
+
+    void testEqualsSameObject() throws Exception {
+        assert s1 == s1
+        assert s1.equals(s1)
+    }
+
+    void testEqualsSameProperties() throws Exception {
+        assert s0 == s1
+        assert s0.equals(s1)
+        assert s1.equals(s0)
+    }
+
+    void testEqualsDifferentString() throws Exception {
+        assert s1 != s2
+        assert !s1.equals(s2)
+        assert !s2.equals(s1)
+    }
+
+    void testEqualsDifferentInteger() throws Exception {
+        assert s1 != s3
+        assert !s1.equals(s3)
+        assert !s3.equals(s1)
+    }
+
+    void testEqualsDifferentMap() throws Exception {
+        assert s1 != s4
+        assert !s1.equals(s4)
+        assert !s4.equals(s1)
+    }
+
+    void testEqualsDifferentList() throws Exception {
+        assert s1 != s5
+        assert !s1.equals(s5)
+        assert !s5.equals(s1)
+    }
+
+    void testEqualsDifferentSet() throws Exception {
+        assert s1 != s6
+        assert !s1.equals(s6)
+        assert !s6.equals(s1)
+    }
+
+    void testEqualsDifferentObject() throws Exception {
+        assert s1 != s7
+        assert !s1.equals(s7)
+        assert !s7.equals(s1)
+    }
+
+    void testHashCode() throws Exception {
+        assert s1.hashCode() == s0.hashCode()
+        assert s1.hashCode() == s1.hashCode()
+        assert s1.hashCode() != s2.hashCode()
+        assert s1.hashCode() != s3.hashCode()
+        assert s1.hashCode() != s4.hashCode()
+        assert s1.hashCode() != s4.hashCode()
+        assert s1.hashCode() != s5.hashCode()
+        assert s1.hashCode() != s6.hashCode()
+        assert s1.hashCode() != s7.hashCode()
+    }
+}
