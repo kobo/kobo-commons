b74b756b195ebe87b4b15fcb756b82abf859b304
Y.Nakano <ynak@jggug.org>
2009-10-26 14:34:53 +0900

    removed codes outputting into standard out

--- src/test/groovy/org/jggug/kobo/commons/lang/EquivTest.groovy
+++ src/test/groovy/org/jggug/kobo/commons/lang/EquivTest.groovy
@@ -28,58 +28,30 @@ class EquivTest extends GroovyTestCase {
     }
 
     void testEqualsSameObject() throws Exception {
-        println "s1: " + s1
-        println s1.class
-        println s1.class.declaredFields*.annotations
-        println "s1 == s1 -> " + (s1 == s1)
-        println "s1.equals(s1) -> " + s1.equals(s1)
         assert s1 == s1
         assert s1.equals(s1)
     }
 
     void testEqualsDifferentIgnored() throws Exception {
-        println "-"*20
-        println "s1: " + s1
-        println "s2: " + s2
-        println "s1 == s2 -> " + (s1 == s2)
-        println "s1.equals(s2) ->" + s1.equals(s2)
-        println "s2.equals(s1) ->" + s2.equals(s1)
         assert s1 == s2
         assert s1.equals(s2)
         assert s2.equals(s1)
     }
 
     void testEqualsDifferentValue() throws Exception {
-        println "-"*20
-        println "s1: " + s1
-        println "s3: " + s3
-        println "s1 == s3 -> " + (s1 == s3)
-        println "s1.equals(s3) ->" + s1.equals(s3)
-        println "s3.equals(s1) ->" + s3.equals(s1)
         assert s1 != s3
         assert !s1.equals(s3)
         assert !s3.equals(s1)
     }
 
     void testEqualsDifferentName() throws Exception {
-        println "-"*20
-        println "s1: " + s1
-        println "s4: " + s4
-        println "s1 == s4 -> " + (s1 == s4)
-        println "s1.equals(s4) ->" + s1.equals(s4)
-        println "s4.equals(s1) ->" + s4.equals(s1)
         assert s1 != s4
         assert !s1.equals(s4)
         assert !s4.equals(s1)
     }
 
     void testHashCode() throws Exception {
-
-        println "-"*20
-        println "s1 -> " + s1.hashCode()
-        println "s2 -> " + s2.hashCode()
-        println "s3 -> " + s3.hashCode()
-        println "s4 -> " + s4.hashCode()
+        assert s1.hashCode() == s1.hashCode()
         assert s1.hashCode() == s2.hashCode()
         assert s1.hashCode() != s3.hashCode()
         assert s1.hashCode() != s4.hashCode()
